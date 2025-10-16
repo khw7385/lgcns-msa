7c74a39d0f35707187ee6086711523976a1d62f6
khw7385 <khw7385@naver.com>
2025-10-16 09:15:39 +0900

    미처 커밋하지 못한 사항 커밋

--- dcompose/deploy/zero_downtown_deploy_test.sh
+++ dcompose/deploy/zero_downtown_deploy_test.sh
@@ -1,9 +1,24 @@
 #!/bin/zsh
 
-URL = "http://localhost:9889/
+URL="http://localhost:9889/"
 
 while true
 do
-	curl -s -o /dev/null -w "%{http_code}\n" $URL
-	sleep 10
+    TIME=$(date "+%Y-%m-%d %H:%M:%S")
+
+    INFO=$(curl -s -o /dev/null -w "%{http_code}" --max-time 3 --retry 3 --retry-delay 1  $URL 2>&1)
+    EXIT_CODE=$?
+
+    if [[ $EXIT_CODE -ne 0 ]]; then
+        echo "(CURL ERROR) $TIME | exit_code=$EXIT_CODE | message=$INFO"
+    else
+        CODE=$(echo "${INFO}")
+        if [[ $CODE -ge 200 && $CODE -lt 300 ]]; then
+            echo "$TIME | HTTP $CODE"
+        else
+            echo "(HTTP ERROR) $TIME | HTTP $CODE"
+        fi
+    fi
+
+    sleep 1
 done
